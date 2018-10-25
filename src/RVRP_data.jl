a53f3bc227b8ccbc1d1061350771acdf2bbd0148
Issam Tahiri <tahiri.inpt@gmail.com>
2018-10-25 21:00:08 +0200

    cleanup of data

--- src/RVRP_data.jl
+++ src/RVRP_data.jl
@@ -1,24 +1,29 @@
-abstract type AbstractNode end
-
 struct ProblemType
     fleet_size::String # INFINITE or FINITE
-    fleet_composition::String # HOMOGENEOUS or HETEROGENEOUS  
+    fleet_composition::String # HOMOGENEOUS or HETEROGENEOUS
 end
 
 struct Coord
     x::Float64
     y::Float64
-end 
+end
 
 struct Location # define: index + distance matrix or Coord
     id::String # optional
-    coord::Coord # optional
     index::Int # optional
+    coord::Coord # optional
 end
 
 struct TimeWindow
     tw_start::Float64
     tw_end::Float64
+end
+
+struct UnitPricing
+    distance_price::Float
+    travel_time_price::Float
+    service_time_price::Float
+    wait_time_price::Float
 end
 
 struct Costs
@@ -29,90 +34,64 @@ struct Costs
     wait::Float64
 end
 
-struct VehicleType
-    id::String
-    capacity::Int
-    costs::Costs
-end
-
-struct Depot <: AbstractNode
+struct Depot
     location::Location
     time_windows::Vector{TimeWindow} # optional
 end
 
-struct Pickup <: AbstractNode
+struct Pickup
+    id::String
+    index::Int
     location::Location
     capacity_demand::Float64
     time_windows::Vector{TimeWindow} # optional
-    duration::Float64 # optional      
-    req_id::String
+    service_time::Float64 # optional
 end
 
-struct Delivery <: AbstractNode
+struct Delivery
+    id::String
+    index::Int
     location::Location
     capacity_demand::Float64
     time_windows::Vector{TimeWindow} # optional
-    duration::Float64 # optional
-    req_id::String
+    service_time::Float64 # optional
 end
 
-struct Operation <: AbstractNode
-    start_location::Location
-    end_location::Location
-    required_capacity::Float64
-    delta_capacity::Float64
-    time_windows::Vector{TimeWindow} # optional
-    duration::Float64 # optional      
-    req_id::String
-end
-
-abstract type AbstractRequest end
-
-struct PickupRequest <: AbstractRequest
+struct Shipment
     id::String
-    node::Pickup
-end    
-
-struct DeliveryRequest <: AbstractRequest
-    id::String
-    node::Delivery
-end    
-
-struct OperationRequest <: AbstractRequest
-    id::String
-    node::Operation
-end    
-
-struct Shipment <: AbstractRequest
-    id::String
+    index::Int
     pickup::Pickup
     delivery::Delivery
 end
 
-abstract type AbstractVehicle end
-
-struct Vehicle <: AbstractVehicle
+struct VehicleType
     id::String
-    depot::Depot # optional
-    v_type::VehicleType
+    index::Int
+    fixed_cost::Float64
+    unit_pricing::UnitPricing
+    capacity::Float64
+end
+
+struct InstanceVehicleType # vehicle type in optimization instance.
+    id::String
+    index::Int
+    depot::Depot # optional. If not mentionned vehicle start from first action
+    vehicle_type::VehicleType
     time_schedule::TimeWindow
     return_to_depot::Bool
-    infinite_copies::Bool
-    initial_load::Float64
-    picked_shipments::Vector{Shipment}
+    nb_of_copies::Int
+    intial_load::Float64
 end
 
 struct RvrpProblem
     problem_id::String
     problem_type::ProblemType
-    vehicles::Vector{Vehicle}
     vehicle_types::Vector{VehicleType}
+    instance_vehicle_types::Vector{InstanceVehicleType}
     distance_matrix::Array{Float64,2}
     travel_times_matrix::Array{Float64,2}
     # Requests
     pickups::Vector{PickupRequest}
     deliveries::Vector{DeliveryRequest}
-    operations::Vector{OperationRequest}
     shipments::Vector{Shipment}
-    picked_shipments::Vector{Shipment}
-end 
+end
