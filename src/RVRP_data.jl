5e2a22d82511227728117df805b8b0d1b1558b01
Vitor Nesello <vitornesello@gmail.com>
2018-10-25 11:46:21 +0200

    siggestion of data structure

--- src/RVRP_data.jl
+++ src/RVRP_data.jl
@@ -1,19 +1,24 @@
 abstract type AbstractNode end
+@enum RESOURCE Time TravelTime WaitingTime Distance TravelDistance Capacity
+@enum OPERATIONTYPE Pickup Delivery Shipment Cleaning
+@enum REQUESTTYPE SingleOperation Shipment
+const ResourceValues = Dict{RESOURCE,Float64}
+
 
 struct ProblemType
     fleet_size::String # INFINITE or FINITE
-    fleet_composition::String # HOMOGENEOUS or HETEROGENEOUS  
+    fleet_composition::String # HOMOGENEOUS or HETEROGENEOUS
 end
 
 struct Coord
     x::Float64
     y::Float64
-end 
+end
 
 struct Location # define: index + distance matrix or Coord
     id::String # optional
+    index::int
     coord::Coord # optional
-    index::Int # optional
 end
 
 struct TimeWindow
@@ -21,98 +26,62 @@ struct TimeWindow
     tw_end::Float64
 end
 
-struct Costs
-    fixed::Float64
-    distance::Float64
-    time::Float64
-    service::Float64
-    wait::Float64
-end
-
-struct VehicleType
-    id::String
-    capacity::Int
-    costs::Costs
-end
-
 struct Depot <: AbstractNode
     location::Location
     time_windows::Vector{TimeWindow} # optional
 end
 
-struct Pickup <: AbstractNode
-    location::Location
-    capacity_demand::Float64
-    time_windows::Vector{TimeWindow} # optional
-    duration::Float64 # optional      
-    req_id::String
-end
-
-struct Delivery <: AbstractNode
-    location::Location
-    capacity_demand::Float64
-    time_windows::Vector{TimeWindow} # optional
-    duration::Float64 # optional
-    req_id::String
-end
-
 struct Operation <: AbstractNode
-    start_location::Location
-    end_location::Location
-    required_capacity::Float64
-    delta_capacity::Float64
+    location::Location
+    consumption::ResourceValues
     time_windows::Vector{TimeWindow} # optional
-    duration::Float64 # optional      
-    req_id::String
+    req_index::Int
+    operation_type::OPERATIONTYPE # Pickup, Delivery, Cleaning, ...
 end
 
-abstract type AbstractRequest end
-
-struct PickupRequest <: AbstractRequest
+struct Request
     id::String
-    node::Pickup
-end    
-
-struct DeliveryRequest <: AbstractRequest
-    id::String
-    node::Delivery
-end    
-
-struct OperationRequest <: AbstractRequest
-    id::String
-    node::Operation
-end    
-
-struct Shipment <: AbstractRequest
-    id::String
-    pickup::Pickup
-    delivery::Delivery
+    index::Int
+    operations::Vector{Operation} # Sequence of operations ; can be limited to one; can be a pair of Pickup and Delivery, or a triplets including a cleaning first, etc
+    request_type::REQUESTTYPE # SingleOperation, Shipment, ...
 end
 
-abstract type AbstractVehicle end
-
-struct Vehicle <: AbstractVehicle
+struct VehicleType
     id::String
+    index::Int
     depot::Depot # optional
-    v_type::VehicleType
+    prices::ResourceValues
+    capacity::ResourceValues
     time_schedule::TimeWindow
     return_to_depot::Bool
-    infinite_copies::Bool
-    initial_load::Float64
-    picked_shipments::Vector{Shipment}
+    initial_state::ResourceValues
 end
 
 struct RvrpProblem
     problem_id::String
     problem_type::ProblemType
-    vehicles::Vector{Vehicle}
     vehicle_types::Vector{VehicleType}
+    nb_vehicles::Dict{VehicleType,Int}
     distance_matrix::Array{Float64,2}
     travel_times_matrix::Array{Float64,2}
+
     # Requests
-    pickups::Vector{PickupRequest}
-    deliveries::Vector{DeliveryRequest}
-    operations::Vector{OperationRequest}
-    shipments::Vector{Shipment}
-    picked_shipments::Vector{Shipment}
-end 
+    # Requests with only one operation of type Pickup:
+    pickups::Vector{Request}
+
+    # Requests with only one operation of type Delivery:
+    deliveries::Vector{Request}
+
+    # Requests with arbitrary number of operations of any OPERATIONTYPE
+    # that must be done in the given sequence, by the same vehicle and
+    # preemption (between operations) is not allowed:
+    operations::Vector{Request}
+
+    # Requests with two operatios: first a Pickup, then a Delivery
+    # with precedence between them, preemption is allowed:
+    shipments::Vector{Request}
+
+    # Requests that are ongoing, where the Int part represents the
+    # number of finished operations in the given request:
+    ongoing_operations::Vector{Pair{Request,Int}}
+end
