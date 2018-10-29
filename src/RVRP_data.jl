0f2364391bf4b9e338d9aadc13b6760f347d0505
Issam Tahiri <tahiri.inpt@gmail.com>
2018-10-29 12:06:07 +0100

    update of data struct after meeting

--- src/RVRP_data.jl
+++ src/RVRP_data.jl
@@ -20,7 +20,7 @@ struct TimeWindow
 end
 
 struct UnitPricing
-    distance_price::Float
+    travel_distance_price::Float
     travel_time_price::Float
     service_time_price::Float
     wait_time_price::Float
@@ -33,7 +33,7 @@ end
 
 struct Pickup
     id::String
-    index::Int # -1 if its the pickup of a shipment
+    index::Int # if its the pickup of a shipment, the index of the shipment
     location::Location
     capacity_demand::Float64
     time_windows::Vector{TimeWindow} # optional
@@ -42,7 +42,7 @@ end
 
 struct Delivery
     id::String
-    index::Int # -1 if its the delivery of a shipment
+    index::Int # if its the pickup of a shipment, the index of the shipment
     location::Location
     capacity_demand::Float64
     time_windows::Vector{TimeWindow} # optional
@@ -54,9 +54,10 @@ struct Shipment
     index::Int
     pickup::Pickup
     delivery::Delivery
+    max_duration::Float64
 end
 
-struct VehicleType
+struct VehicleCategory
     id::String
     index::Int
     fixed_cost::Float64
@@ -64,24 +65,28 @@ struct VehicleType
     capacity::Float64
 end
 
-struct InstanceVehicleType # vehicle type in optimization instance.
+struct HomogeneousVechicleSet # vehicle type in optimization instance.
     id::String
     index::Int
-    depot::Depot # optional. If not mentionned vehicle start from first action
-    vehicle_type::VehicleType
-    time_schedule::TimeWindow
-    return_to_depot::Bool
-    nb_of_copies::Int
+    departure_depot_index::Int # If -1 mentionned vehicle start from first action
+    arrival_depot_indices::Vector{Int}
+    vehicle_category::VehicleCategory
+    working_time_window::TimeWindow
+    min_nb_of_vehicles::Int
+    max_nb_of_vehicles::Int
     intial_load::Float64
+    max_travel_time::Float64
+    max_travel_distance::Float64
 end
 
 struct RvrpProblem
     problem_id::String
     problem_type::ProblemType
-    vehicle_types::Vector{VehicleType}
-    instance_vehicle_types::Vector{InstanceVehicleType}
-    distance_matrix::Array{Float64,2}
-    travel_times_matrix::Array{Float64,2}
+    vehicle_categories::Vector{VehicleCategory}
+    vehicle_sets::Vector{HomogeneousVechicleSet}
+    travel_distance_matrix::Array{Float64,2}
+    travel_time_matrix::Array{Float64,2}
+    depots::Vector{Depot}
     # Requests
     pickups::Vector{Pickup}
     deliveries::Vector{Delivery}
