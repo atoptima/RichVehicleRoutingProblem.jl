5a0345e60d7c9e42107c9908f8c96c9d6bcc4370
fD-V <fv@math.u-bordeaux.fr>
2018-11-06 10:31:13 +0100

    compartment details

--- src/RVRP_data.jl
+++ src/RVRP_data.jl
@@ -3,7 +3,7 @@ struct Coord
     y::Float64
 end
 
-mutable struct Location # define: index + distance matrix or Coord
+mutable struct Location
     id::String
     index::Int # Not given in JSON
     coord::Coord # optional
@@ -14,7 +14,7 @@ struct TimeWindow
     end_time::Float64
 end
 
-struct UnitPricing
+struct UnitPrices # cost per unit
     travel_distance_price::Float64
     travel_time_price::Float64
     service_time_price::Float64
@@ -25,14 +25,14 @@ mutable struct Depot
     id::String
     index::Int # Not given in JSON
     location::Location
-    time_windows::Vector{TimeWindow} # optional
+    opening_time_windows::Vector{TimeWindow} # optional
 end
 
 mutable struct PickupPoint
     id::String # If its part of a shipment, it has is own id anyway
     index::Int # Not given in JSON. 
     location::Location
-    time_windows::Vector{TimeWindow} # optional
+    opening_time_windows::Vector{TimeWindow} # optional
     service_time::Float64 # optional; to measure access time for instance
 end
 
@@ -41,7 +41,7 @@ mutable struct DeliveryPoint
     id::String # If it is part of a shipment,it has is own id anyway
     index::Int # Not given in JSON. 
     location::Location
-    time_windows::Vector{TimeWindow} # optional
+    opening_time_windows::Vector{TimeWindow} # optional
     service_time::Float64 # optional; to measure access time for instance
 end
 
@@ -52,31 +52,31 @@ mutable struct ProductCategory
     prohibited_predecessor_product_ids::Vector{String}  # if any
 end
 
-mutable struct SpecificProduct 
+mutable struct SpecificProduct # an entity to understand as a commodity in a multi-commodity flow problem
     id::String 
     index::Int # Not given in JSON. 
     product_category_id::String
-    pickup_availabitilies::Dict{String,Float64} # link PickupPoint ids with available quantities; undefined if no restrictions, i.e, if available in any PickupPoint in large quantities
-    delivery_capacities::Dict{String,Float64} # link DeliveryPoint ids with available capacities; undefined if no restrictions, i.e, if can go to any DeliveryPoint in large quantities
+    pickup_availabitilies::Dict{String,Float64} # undefined if no restrictions, i.e, if available in any PickupPoint in large quantities
+    delivery_capacities::Dict{String,Float64} # undefined if no restrictions, i.e, if one can deliver to any DeliveryPoint in large quantities
 end
 
 mutable struct ShipmentRequest
     id::String
     index::Int # Not given in JSON
-    request_type::Int # 1 = pickup, 2 = delivery, 3 = pickup&delivery, 4 = complex request
+    request_type::Int # 1 = pickup, 2 = delivery, 3 = pickup&delivery, 4 = complex_request
     specific_product_id::String  
     is_optional::Bool  # default is false
     price_reward::Float64 # if is_optional
     quantity::Float64 # of the shipment/pickup/delivery
-    split_fulfilment::Bool  # default is false
+    load_capacity_conso::Float64 # portion of the vehicle load capacity used by the request (can be equal to the quantity)
+    split_fulfillment::Bool  # default is false
     precedence_restriction::Int # default is 0 = no restriction; 1 after all pure pickups, 2 after all pure deliveries, 3 if cannot follow a product that is in the prohibited predecessor list
-    alternative_pickup_point_ids::Vector{String} # provide the ids of PickupPoints; defined only if it is a subset of SpecificProduct sources; singleton for request_type = pickup or pickup&delivery
-    alternative_delivery_point_ids::Vector{String} #  provide the ids of DeliveryPoints; defined only if it is a subset of SpecificProduct destinations; singleton for request_type = delivery or pickup&delivery
+    alternative_pickup_point_ids::Vector{String} # defined only if it is a subset of SpecificProduct pickup sources; it is a singleton for request_type = pickup or pickup&delivery
+    alternative_delivery_point_ids::Vector{String} # defined only if it is a subset of SpecificProduct delivery destinations; it is a singleton for request_type = delivery or pickup&delivery
     setup_service_time::Float64 # optional; on top of PickupPoint service_time; used to measure pre-cleaning or loading time for instance
     setdown_service_time::Float64 # optional; on top of DeliveryPoint service_time; used to measure post-cleaning or unloading time for instance
     max_duration::Float64 # used for the dail-a-ride model or similar applications
 end
-
 
 mutable struct VehicleCategory
     id::String
@@ -84,7 +84,8 @@ mutable struct VehicleCategory
     fixed_cost::Float64
     unit_pricing::UnitPricing
     compartment_capacities::Vector{Float64} # the santard case is to have a single compartment
-    prohibited_product_ids::Vector{String}  # if any
+    loading_option::Int # 0 = no restrictions, 1 = one request per compartment, 2 = removable compartment separation (note that product conflicts are measured within a compartment)
+    prohibited_productCategory_ids::Vector{String}  # if any
 end
 
 mutable struct HomogeneousVehicleSet # vehicle type in optimization instance.
@@ -116,7 +117,6 @@ struct RvrpInstance
     commodities::Vector{Commodity}
     requests::Vector{ShipmentRequest}
 end
-
 
 mutable struct RvrpComputedData
     instance_id::String
