69bc227882d0bc796db315ac7f790a359c4bd0cb
Issam Tahiri <tahiri.inpt@gmail.com>
2018-11-09 11:16:09 +0100

    location -> OperationPoint locationGroup -> OperationGroup pickup_locations_id -> pickup_point_or_group_id (same of delivery)

--- src/RVRP_data.jl
+++ src/RVRP_data.jl
@@ -1,26 +1,22 @@
-struct Coord
-    x::Float64
-    y::Float64
-end
-
 struct TimeWindow
     begin_time::Float64
     end_time::Float64
 end
 
-mutable struct Location # can be a Depot, Pickup, Delivery, Recharging, or a combination of those
+mutable struct OperationPoint # can be a Depot, Pickup, Delivery, Recharging, or a combination of those
     id::String
-    coord::Coord
+    x_coord::Float64
+    y_coord::Float64
     opening_time_windows::Vector{TimeWindow}
     access_time::Float64
     energy_fixed_cost::Float64 # for entry fee, if any
     energy_unit_cost::Float64 # for recharging cost per unit of energy, if any
-    energy_recharging_speeds::Vector{Float64} # at index i, the i-th energy interval recharging speed (energy_units per time_unit). empty if no recharing in the location.
+    energy_recharging_speeds::Vector{Float64} # at index i, the i-th energy interval recharging speed (energy_units per time_unit). empty if no recharing in the point.
 end
 
-mutable struct LocationGroup # to identify a set of Locations with some commonalities, such as all possible Pickup Locations for a product.
+mutable struct OperationGroup # to identify a set of operation points with some commonalities, such as all possible Pickup Points for a product.
     id::String
-    location_ids::Vector{String}
+    operation_point_ids::Vector{String}
 end
 
 mutable struct ProductCategory
@@ -32,21 +28,21 @@ end
 mutable struct SpecificProduct
     id::String
     product_category_id::String
-    pickup_locations_id::String # id of the single Location or id of a LocationGroup of pickup
-    pickup_availabitilies::Vector{Float64} # availabilities listed in the same order as pickup locations
-    delivery_locations_id::String # id of the single Location or id of a LocationGroup of delivery
-    delivery_capacities::Vector{Float64} # capacities listed in the same order as delivery locations
+    pickup_point_or_group_id::String # id of an OperationPoint or id of an OperationGroup for picking up this product
+    delivery_point_or_group_id::String # id of an OperationPoint or id of a OperationGroup for delivering this product
+    pickup_availabitilies::Vector{Float64} # listed in the same order as operation_point_ids in the pickup group, if length > 1
+    delivery_capacities::Vector{Float64} # listed in the same order as operation_point_ids in the delivery group, if length > 1
 end
 
 mutable struct Request # can be
-    # a specific product shipment from a depot to a DeliveryPoint, or
-    # a specific product shipment from a PickupPoint to a depot, or
-    # a specific product delivery-only to one of several DeliveryPoints. Pickup is handled by another request(s).
-    # a specific product pickup-only from one of several PickupPoints. Delivery is handled by another request(s).
-    # a specific product shipment from a specific PickupPoint to a specific DeliveryPoint, or
-    # a specific product shipment from one of several PickupPoints to a specific DeliveryPoint, or
-    # a specific product shipment from a specific PickupPoints to one of several DeliveryPoints, or
-    # a specific product shipment from one of several PickupPoints to one of several DeliveryPoints.
+    # a specifi product shipment from a depot to a DeliveryPoint, or
+    # a specifi product shipment from a PickupPoint to a depot, or
+    # a specifi product delivery-only to one of several DeliveryPoints. Pickup is handled by another request(s).
+    # a specifi product pickup-only from one of several PickupPoints. Delivery is handled by another request(s).
+    # a specifi product shipment from a specific PickupPoint to a specific DeliveryPoint, or
+    # a specifi product shipment from one of several PickupPoints to a specific DeliveryPoint, or
+    # a specifi product shipment from a specific PickupPoints to one of several DeliveryPoints, or
+    # a specifi product shipment from one of several PickupPoints to one of several DeliveryPoints.
     id::String
     specific_product_id::String
     is_optional::Bool  # default is false
@@ -55,8 +51,8 @@ mutable struct Request # can be
     compartment_capacity_consumption::Float64 # portion of the vehicle compartment capacity used by the request (it can be equal to the quantity, or different even in terms of unit: volume versus weight for instance).
     split_fulfillment::Bool  # true if split delivery/pickup is allowed, default is false
     precedence_restriction::Int # default is 0 = only predecessor restrictions; 1 after all pickups, 2 after all deliveries.
-    alternative_pickup_locations_id::String # empty string for delivery-only requests. id of the single Location or id of a LocationGroup of pickup
-    alternative_delivery_locations_id::String # empty string for pickup-only requests. id of the single Location or id of a LocationGroup of delivery
+    pickup_point_or_group_id::String # empty string for delivery-only requests. id of the OperationPoint or id of an OperationGroup representing alternatives for pickup
+    delivery_point_or_group_id::String # empty string for pickup-only requests. id of the OperationPoint or id of a OperationGroup representing alternatives for delivery
     pickup_service_duration::Float64 # used to measure pre-cleaning or loading time for instance
     delivery_service_duration::Float64 # used to measure post-cleaning or unloading time for instance
     max_duration::Float64 # used for the dial-a-ride model or similar applications
@@ -78,8 +74,8 @@ end
 mutable struct HomogeneousVehicleSet # vehicle type in optimization instance.
     id::String
     vehicle_category_id::String
-    alternative_departure_locations_id::String # []: Vehicle starts directly from first action of first request
-    alternative_arrival_locations_id::String # []: Open routes (vehicles not required to return to depots)
+    departure_point_or_group_id::String # []: Vehicle starts directly from first action of first request
+    arrival_point_or_group_id::String # []: Open routes (vehicles not required to return to depots)
     working_time_window::TimeWindow
     initial_energy_charge::Float64
     min_nb_of_vehicles::Int
@@ -94,8 +90,8 @@ struct RvrpInstance
     travel_distance_matrix::Array{Float64,2}
     travel_time_matrix::Array{Float64,2}
     energy_consumption_matrix::Array{Float64,2}
-    locations::Vector{Location}
-    location_groups::Vector{LocationGroup}
+    operation_points::Vector{OperationPoint}
+    operation_groups::Vector{OperationGroup}
     product_categories::Vector{ProductCategory}
     specific_products::Vector{SpecificProduct}
     requests::Vector{Request}
