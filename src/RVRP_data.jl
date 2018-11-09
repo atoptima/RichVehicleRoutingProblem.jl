5e32381ad8044d77f960dfac7785437d7e943fd4
Vitor Nesello <vitornesello@gmail.com>
2018-11-09 19:13:36 +0100

    changing the way to model compartment-product incompatibility

--- src/RVRP_data.jl
+++ src/RVRP_data.jl
@@ -12,7 +12,7 @@ mutable struct OperationPoint # can be a Depot, Pickup, Delivery, Recharging, ..
     access_time::Float64
     energy_fixed_cost::Float64 # an entry fee, if any
     energy_unit_cost::Float64 # recharging cost per unit of energy, if any
-    energy_recharging_speeds::Vector{Float64} # if recharging in this point: the i-th seep is associted to the i^th energy interval defined for the vehicle
+    energy_recharging_speeds::Vector{Float64} # if recharging in this point: the i-th speep is associted to the i-th energy interval defined for the vehicle
 end
 
 mutable struct OperationGroup # to identify a set of operation points with some commonalities, such as all possible pickups for a request.
@@ -29,8 +29,8 @@ end
 mutable struct SpecificProduct
     id::String
     product_category_id::String
-    pickup_availabitilies_at_point_or_group_ids::Dict{String,Float64} # defined only if pickup points have a restricted capacity 
-    delivery_capacities_at_point_or_group_ids::Dict{String,Float64}  # defined only if delivery points have a restricted capacity 
+    pickup_availabitilies_at_point_or_group_ids::Dict{String,Float64} # defined only if pickup points have a restricted capacity
+    delivery_capacities_at_point_or_group_ids::Dict{String,Float64}  # defined only if delivery points have a restricted capacity
 end
 
 mutable struct Request # can be
@@ -46,8 +46,8 @@ mutable struct Request # can be
     specific_product_id::String
     split_fulfillment::Bool  # true if split delivery/pickup is allowed, default is false
     precedence_status::Int # default = 0 = no restiction, 1 = product predecessor restrictions; 2= after all pickups,3=  after all deliveries.
-    mantadory_status::Int # default = 0 = mandatory, 1= semi_mandatory (must be covered in a feasible solution exists), 2 = optional
-    price_reward::Float64 # define if semi_mandatory or optional
+    mantadory_status::Int # default = 0 = mandatory, 1= semi_mandatory (must be covered if a feasible solution exists), 2 = optional
+    price_reward::Float64 # define if semi_mandatory or optional; reward for fulfilling the request
     product_quantity::Float64 # of the request
     shipment_capacity_consumption::Vector{Float64} # can include several independant capacity consumptions: as weight, value, volume
     shipment_measures::Vector{Float64} # can include several independant measures: as heigh, depth, width
@@ -66,7 +66,7 @@ mutable struct VehicleCategory
     service_time_unit_price::Float64
     waiting_time_unit_price::Float64
     compartment_capacities::Array{Float64,2} # matrix providing capacites for each compartment the additive measure: weight, value, volume
-    compartment_dimensions::Array{Float64,2} # matrix providing for each compartment their significant dimension measure: heigh, depth, width; used to test compatibility
+    prohibited_product_categories::Dict{Int,Vector{String}} # key: index of compartment, value: vector of ids of product categories that cannot be transpoted in compartment 'key'
     energy_interval_lengths::Vector{Float64} # at index i, the length of the i-th energy interval. empty if no recharging.
     loading_option::Int # 0 = no restriction (=default), 1 = one request per compartment, 2 = removable compartment separation (note that product conflicts are measured within a compartment)
     prohibited_product_category_ids::Vector{String}  # if any
