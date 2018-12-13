c25470226eda1c019755e697eb56c23590ad427c
fD-V <fv@math.u-bordeaux.fr>
2018-12-13 06:43:16 +0100

    removing entries that are postcomputed to keep them for High Level Apps & adding quantity

--- src/RVRP_solution.jl
+++ src/RVRP_solution.jl
@@ -3,23 +3,23 @@
 mutable struct Action
     id::String
     location_id::String
-    operation_type::Int # 0- Depot, 1 - Pickup, 2 - Delivery, 3 - otherOperation # should be picked quantity
     request_id::String # if any request is linked to a request the operation
-    scheduled_start_time::Float64 # post-computed
+    operation_type::Int # 0- Depot, 1 - Pickup, 2 - Delivery, 3 - ...otherOperation
+    product_quantity::Float64 # to record the picked/delivered quantity in case of split_fulfillment
 end
 
 mutable struct Route
     id::String
     vehicle_set_id::String
     sequence::Vector{Action}
-    end_status::Int # 0 returnToStartDepot, 1 returnToOtherDepot, 2 ongoing # post-computed
     # path::OSRMpath     # TODO: add OSRM path here
 end
 
 mutable struct RvrpSolution
     id::String
     instance_id::String
-    cost::Float64 # post-compute
+    cost::Float64 
     routes::Vector{Route}
-    unassigned_request_ids::Vector{String} # among semi-mandatory or optional request # post-computed
 end
+
+
