250a58185ad57685ae03f545cb8b57945921af6d
fD-V <fv@math.u-bordeaux.fr>
2018-12-13 06:30:58 +0100

    new branch on solution

--- src/RVRP_solution.jl
+++ src/RVRP_solution.jl
@@ -5,14 +5,14 @@ struct Action
     location_id::String
     operation_type::Int # 0- Depot, 1 - Pickup, 2 - Delivery, 3 - otherOperation # should be picked quantity
     request_id::String # if any request is linked to a request the operation
-    scheduled_start_time::Float64 # post-compute
+    scheduled_start_time::Float64 # post-computed
 end
 
 struct Route
     id::String
     vehicle_set_id::String
     sequence::Vector{Action}
-    end_status::Int # 0 returnToStartDepot, 1 returnToOtherDepot, 2 ongoing # post-compute
+    end_status::Int # 0 returnToStartDepot, 1 returnToOtherDepot, 2 ongoing # post-computed
     # path::OSRMpath     # TODO: add OSRM path here
 end
 
@@ -21,5 +21,5 @@ struct RvrpSolution
     instance_id::String
     cost::Float64 # post-compute
     routes::Vector{Route}
-    unassigned_request_ids::Vector{String} # among semi-mandatory or optional request # post-compute
+    unassigned_request_ids::Vector{String} # among semi-mandatory or optional request # post-computed
 end
